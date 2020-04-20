f2e983fd4426764762d341397e40ba5e3b2b106a
erkanturker <erkanturker17@gmail.com>
2020-04-20 16:15:30 -0400

    Initial Commit

--- src/day01_FirstProgramming/hiWorld.java
+++ src/day01_FirstProgramming/hiWorld.java
@@ -1,4 +1,9 @@
 package day01_FirstProgramming;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class hiWorld {
+    public static void main (String[] args) {
+        System.out.println ("hi");
+    }
 }
